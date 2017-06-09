fbd84978a65cedcbbd9e3e1d347b729d2707c521
csapanda <arun.joseph@csa.iisc.ernet.in>
2017-06-09 10:30:11 +0530

    basic message vector is ready

---
+++ primitives/headers/msgvector.hpp
@@ -0,0 +1,219 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2017 MPC-PRIMITIVES ()
+* This file is part of the MPC-PRIMTIVES project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on MPC-PRIMTIVES contain an appropriate citation to MPC-PRIMITIVES, including a reference to
+* 
+* 
+* MPC-PRIMITIVES uses several open source libraries. Please see these projects for any further licensing issues.cal
+* For more information , See https://raw.githubusercontent.com/cris-iisc/mpc-primitives/master/LICENSE.MD
+*
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+
+#ifndef MSGVECTOR_H
+#define MSGVECTOR_H
+#include "common.hpp"
+#include <math.h>
+
+//Some constants neede, can be shifted together to create 'typedefs.hpp' if needed
+#define MSG_BLOCK_BITS	128
+#define MSG_BLOCK_BYTE 16
+
+#define getIntBitsFromLen(x, from, len) 	( ( (x & ( ( (2<<(len))-1) << from )) >> from) & 0xFF)
+#define getMask(len) 				(( (1<<(len))-1))
+
+static const byte MASK_BIT[8] =
+{ 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1 };
+
+static const byte BIT[8] =
+{ 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80 };
+
+static const byte CMASK_BIT[8] =
+{ 0x7f, 0xbf, 0xdf, 0xef, 0xf7, 0xfb, 0xfd, 0xfe };
+
+static const byte MASK_SET_BIT_C[2][8] =
+{ {0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1},{0,0,0,0,0,0,0,0} };
+
+static const byte C_BIT[8] =
+	{ 0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F };
+
+static const byte SET_BIT_C[2][8] =
+{ {0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80},{0,0,0,0,0,0,0,0} };
+
+//class similar to cbitvector, abstracts message block in a nice way
+class MsgVector{
+	private:
+		byte *msg_ptr;
+		int msg_size;
+		int msg_bit_size;
+
+	public:
+		//default constructor
+		MsgVector(){
+			create();
+		}
+		MsgVector(int bits){
+			create(bits);
+		}
+		//overloaded create functions
+		//default initialization setting size to 0 and ptr to NULL
+		void create(){
+			msg_ptr = NULL;
+			msg_size = 0;
+		}
+		void create(int bits);
+
+		//TODO: void create(int bits, randomness) initialize random bits
+
+		void createBytes(int bytes){
+			create(bytes<<3);
+		}
+		void createZeros(int bits){
+			create(bits);
+			memset(msg_ptr,0,msg_size);
+		}
+
+		/*
+		 * Management operations *
+		 */
+		void reset() {
+			memset(msg_ptr, 0, msg_size);
+		}
+		void resetFromTo(int frombyte, int tobyte) {
+			assert(frombyte>tobyte);
+			memset(msg_ptr+frombyte, 0x00, tobyte-frombyte);
+		}
+		void setToOne() {
+			memset(msg_ptr, 0xFF, msg_size);
+		}
+		int getSize(){
+			return msg_size;
+		}
+		byte* getArray(){
+			return msg_ptr;
+		}
+		void resizeinBytes(int newsize);
+		bool isEqual(MsgVector& vec);
+		bool isEqual(MsgVector& vec, int from, int to);
+		//invert complete message
+		void invert();
+
+
+		/*
+		 * Copy operations *
+		 */
+		void copy(MsgVector& vec){
+			copy(vec.getArray(), 0, vec.getSize());
+		}
+		void copy(MsgVector& vec, int pos, int len){
+			copy(vec.getArray(), pos, len);
+		}
+		void copy(byte* p, int pos, int len);
+
+
+
+		//void XOR_no_mask(int p, int bitPos, int bitLen);
+		//unsigned int GetInt(int bitPos, int bitLen);
+
+		
+		/*
+		 * Bitwise operations
+		 */
+		byte getBit(int idx){
+			return !!(msg_ptr[idx >> 3] & MASK_BIT[idx & 0x7]);
+		}
+		void setBit(int idx, byte b){
+			msg_ptr[idx >> 3] = (msg_ptr[idx >> 3] & CMASK_BIT[idx & 0x7]) | MASK_SET_BIT_C[!b][idx & 0x7];
+		}
+		void xorBit(int idx, byte b){
+			msg_ptr[idx >> 3] ^= MASK_SET_BIT_C[!b][idx & 0x7];
+		}
+		void andBit(int idx, byte b){
+			if (!b) msg_ptr[idx >> 3] &= CMASK_BIT[idx & 0x7];
+		}
+		
+		//used to access bits in the regular order
+		byte getBitNoMask(int idx){
+			return !!(msg_ptr[idx >> 3] & BIT[idx & 0x7]);
+		}
+		void setBitNoMask(int idx, byte b){
+			msg_ptr[idx >> 3] = (msg_ptr[idx >> 3] & C_BIT[idx & 0x7]) | SET_BIT_C[!b][idx & 0x7];
+		}
+		void xorBitNoMask(int idx, byte b){
+			msg_ptr[idx >> 3] ^= SET_BIT_C[!b][idx & 0x7];
+		}
+		void andBitNoMask(int idx, byte b){
+			if (!b) msg_ptr[idx >> 3] &= C_BIT[idx & 0x7];
+		}
+
+
+		/*
+		 * Single byte operations
+		 */
+		void setByte(int idx, byte p) {
+			msg_ptr[idx] = p;
+		}
+		byte getByte(int idx){
+			return msg_ptr[idx];
+		}
+		void xorByte(int idx, byte b){
+			msg_ptr[idx] ^= b;
+		}
+		void andByte(int idx, byte b){
+			msg_ptr[idx] &= b;
+		}
+		void orByte(int pos, byte p){
+			msg_ptr[pos] |=p;
+		}
+
+		/*
+		 * Get Operations
+		 */
+		//void getBits(byte* p, int pos, int len);
+		//void getBytes(byte* p, int pos, int len);
+		//template <class T> void getBytes(T* dst, T* src, T* lim);
+		/*template <class T> T get(int pos, int len){
+			T val = 0;
+			getBits((byte*)&val, pos, len);
+			return val;
+		}*/
+
+		/*
+		 * Set Operations
+		 */
+		/*
+		void setBits(byte* p, int pos, int len);
+		void setBytes(byte* p, int pos, int len);
+		template <class T> void setBytes(T* dst, T* src, T* lim);
+		template <class T> void set(T val, int pos, int len){
+			setBits((byte*)&val, pos, len);
+		}
+		*/
+		void setBitsToZero(int bitpos, int bitlen);
+
+		//Print functions
+		//todo: printBinaryMasked and printContent
+		void print(int fromBit, int toBit);
+		void printHex();
+		void printHex(int fromByte, int toByte);
+		void printBinary() { print(0, msg_size<<3); }
+		void printContent();
+		void printBinaryMasked(int from, int to);
+};
+
+#endif //MSGVECTOR_H
---
+++ primitives/src/msgvector.cpp
@@ -0,0 +1,169 @@
+/**
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+* Copyright (c) 2017 MPC-PRIMITIVES ()
+* This file is part of the MPC-PRIMTIVES project.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* We request that any publication and/or code referring to and/or based on MPC-PRIMTIVES contain an appropriate citation to MPC-PRIMITIVES, including a reference to
+* 
+* 
+* MPC-PRIMITIVES uses several open source libraries. Please see these projects for any further licensing issues.
+* For more information , See https://raw.githubusercontent.com/cris-iisc/mpc-primitives/master/LICENSE.MD
+*
+* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+* 
+*/
+
+#include "../headers/msgvector.hpp"
+
+int ceilDivide(int a, int b){
+	return (1 + (a-1)/b);
+}
+/*Create Overloaded functions*/
+	void MsgVector::create(int bits){
+		if (bits == 0)
+			bits = MSG_BLOCK_BITS;
+
+		int size = ceilDivide( bits, MSG_BLOCK_BITS );
+
+		if (msg_size>0)	free(msg_ptr);
+		msg_size = size*MSG_BLOCK_BYTE;
+		msg_bit_size = bits;
+		msg_ptr = (byte*) calloc(msg_size,sizeof(byte));
+		assert(msg_ptr != NULL);
+	}
+ 
+
+/* Manage functions implementations*/
+void MsgVector::resizeinBytes(int newsize)
+{
+	//Why not realloc
+	//TODO: check that
+	byte* t_ptr = msg_ptr;
+	int t_size = msg_size;
+
+	msg_size = newsize;
+	msg_ptr = (byte*)malloc(sizeof(byte) * msg_size);
+	if (msg_ptr == NULL)
+	{
+		cerr << "Memory allocation failed in CBitVector, requested size: " <<
+			msg_size << " bytes" << endl;
+		exit(0);
+	}
+
+	memcpy(msg_ptr, t_ptr, t_size);
+
+	free(t_ptr);
+}
+bool MsgVector::isEqual(MsgVector& vec){
+	if(msg_size != vec.getSize()){
+		return false;
+	}
+	byte *msg = vec.getArray();
+	for(int i=0;i<msg_size;i++){
+		if(msg_ptr[i] != msg[i])
+			return false;
+	}
+	return true;
+}
+
+bool MsgVector::isEqual(MsgVector& vec, int from, int to){
+	if(vec.getSize() * 8 < to || msg_size * 8 < to || from > to){
+		return false;
+	}
+	for(int i=from;i<to;i++){
+		if(vec.getBit(i)!=getBit(i))
+			return false;
+	}
+	return true;
+}
+void MsgVector::invert(){
+	for(int i=0;i<msg_size;i++){
+		msg_ptr[i] = ~msg_ptr[i];
+	}
+}
+
+
+/*Copy functions*/
+	void MsgVector::copy(byte* p, int pos, int len){
+		if (pos + len > msg_size)
+		{
+			if (msg_ptr)
+				resizeinBytes(pos + len);
+			else
+				createBytes(pos + len);
+		}
+		memcpy(msg_ptr + pos, p, len);
+	}
+
+void MsgVector::setBitsToZero(int bitpos, int bitlen){
+	int firstlim = ceilDivide(bitpos, 8);
+	int firstlen = ceilDivide(bitlen - (bitpos % 8), 8);
+	for (int i = bitpos; i < firstlim; i++)
+	{
+		setBitNoMask(i, 0);
+	}
+	if (bitlen > 7)
+	{
+		memset(msg_ptr + firstlim, 0, firstlen);
+	}
+	for (int i = (firstlim + firstlen) << 8; i < bitpos + bitlen; i++)
+	{
+		setBitNoMask(i, 0);
+	}
+}
+
+
+/*PRINT METHODS*/
+	/*print from index fromBit to index toBit*/
+	void MsgVector::print(int fromBit, int toBit)
+	{
+		if (toBit>(msg_size << 3))
+			cout<< "Not in range"<<endl;
+		else
+		{
+			for (int i = fromBit; i < toBit; i++)
+				cout << (unsigned int) getBitNoMask(i);
+			cout << endl;
+		}
+	}
+
+	void MsgVector::printHex()
+	{
+		for(int i=0;i<msg_size;i++)
+			cout << setw(2) << setfill('0') << (hex) << ((unsigned int) msg_ptr[i]);
+		cout<<endl;
+	}
+
+	void MsgVector::printHex(int fromByte, int toByte)
+	{
+		if(toByte>msg_size)
+			cout<<"Not in range"<<endl;
+		else
+		{
+			for (int i = fromByte; i < toByte; i++)
+				cout << setw(2) << setfill('0') << (hex) << ((unsigned int) msg_ptr[i]);
+			cout << endl;
+		}
+	}
+
+	void MsgVector::printContent()
+	{
+
+	}
+
+	void MsgVector::printBinaryMasked(int from, int to)
+	{
+
+	}
